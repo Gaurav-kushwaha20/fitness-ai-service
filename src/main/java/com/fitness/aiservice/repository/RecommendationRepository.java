0ec5cd8709c9e756b1c23c21e4e3dfc3ef6207ad
Gaurav-kushwaha20 <gaurav.kushwaha.0825@gmail.com>
2025-09-25 13:44:33 +0545

    recomendation api

---
+++ src/main/java/com/fitness/aiservice/repository/RecommendationRepository.java
@@ -0,0 +1,14 @@
+package com.fitness.aiservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.fitness.aiservice.model.Recommendation;
+
+public interface RecommendationRepository extends MongoRepository<Recommendation, String> {
+    List<Recommendation> findByUserId(String userId);
+
+    Optional<Recommendation> findByActivityId(String activityId);
+}
